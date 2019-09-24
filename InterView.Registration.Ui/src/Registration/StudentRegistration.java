e41f50438f23865bb4fa8b63bad3381f4fdff020
vikas maurya <vikas.maurya@sstsinc.com>
2019-09-24 16:38:33 +0530

    hi
    hi

--- InterView.Registration.Ui/src/Registration/StudentRegistration.java
+++ InterView.Registration.Ui/src/Registration/StudentRegistration.java
@@ -42,6 +42,7 @@ public class StudentRegistration {
 		
 		StudentRegistration registration=new StudentRegistration();
 		registration.register();
+		System.out.println("next hi.......................");
 		
 		
 	}
