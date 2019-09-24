a93b560d9a6aeed2e97bc891f0041cec4b0af721
vikas maurya <vikas.maurya@sstsinc.com>
2019-09-24 23:04:25 +0530

    getter generate in mcqQue.java

--- InterView.BussinessLayer/src/questions/logic/layer/MCQQuestions.java
+++ InterView.BussinessLayer/src/questions/logic/layer/MCQQuestions.java
@@ -8,22 +8,42 @@ import MCQ.Db.MCQDataBase;
 import questions.Utility.QuestionsPojo;
 
 public class MCQQuestions {
-	
-	public QuestionsPojo mypojo=null;
-	
-	public MCQQuestions(QuestionsPojo pojo) {
-		this.mypojo=pojo;
-	}
-	
-	
 
-	public static List<MCQQuestions> showQuestions() throws ClassNotFoundException, SQLException
-	{
-		MCQDataBase mcqDB=new MCQDataBase();
-		List<QuestionsPojo> questionPojo=mcqDB.getQuestions();
-		List<MCQQuestions> questions=new ArrayList<>();
-		for (QuestionsPojo pojo : questionPojo) 
-		{
+	public QuestionsPojo mypojo = null;
+
+	public MCQQuestions(QuestionsPojo pojo) {
+		this.mypojo = pojo;
+	}
+
+	public String getQuesion() {
+		return this.mypojo.question;
+	}
+
+	public String getOptionA() {
+		return this.mypojo.optionA;
+	}
+
+	public String getOptionB() {
+		return this.mypojo.optionB;
+	}
+
+	public String getOptionC() {
+		return this.mypojo.optionC;
+	}
+
+	public String getOptionD() {
+		return this.mypojo.optionD;
+	}
+
+	public String getCurrectAnswer() {
+		return this.mypojo.correctOption;
+	}
+
+	public static List<MCQQuestions> showQuestions() throws ClassNotFoundException, SQLException {
+		MCQDataBase mcqDB = new MCQDataBase();
+		List<QuestionsPojo> questionPojo = mcqDB.getQuestions();
+		List<MCQQuestions> questions = new ArrayList<>();
+		for (QuestionsPojo pojo : questionPojo) {
 			questions.add(new MCQQuestions(pojo));
 		}
 		return questions;
