5ae8e95d1ad33860b01e5a36cb2fedb09c474302
vikas maurya <vikas.maurya@sstsinc.com>
2019-09-25 15:08:41 +0530

    ut

--- InterView.BussinessLayer/src/interview/logic/layer/InterViwer.java
+++ InterView.BussinessLayer/src/interview/logic/layer/InterViwer.java
@@ -7,22 +7,21 @@ import interviewer.Utility.InterViwerPojo;
 import interviwer.Db.InterviwerDb;
 
 public class InterViwer {
-	//new interviewer registration
-	public void createRecord(String name,String email,String address,String phoneNumber) throws ClassNotFoundException, SQLException
-	{
-		InterViwerPojo interviwerPojo=new InterViwerPojo();
-		interviwerPojo.name=name;
-		interviwerPojo.email=email;
-		interviwerPojo.address=address;
-		interviwerPojo.phoneNumber=phoneNumber;
-		
-		InterviwerDb interviwerDb=new InterviwerDb();
+	// new interviewer registration
+	public void createRecord(String name, String email, String address, String phoneNumber)
+			throws ClassNotFoundException, SQLException {
+		InterViwerPojo interviwerPojo = new InterViwerPojo();
+		interviwerPojo.name = name;
+		interviwerPojo.email = email;
+		interviwerPojo.address = address;
+		interviwerPojo.phoneNumber = phoneNumber;
+
+		InterviwerDb interviwerDb = new InterviwerDb();
 		interviwerDb.insertingInterviwerRecord(interviwerPojo);
 	}
-	
-	public void Questions() throws ClassNotFoundException, SQLException
-	{
-		MCQDataBase db=new MCQDataBase();
+
+	public void Questions() throws ClassNotFoundException, SQLException {
+		MCQDataBase db = new MCQDataBase();
 		db.retrieveQuestions();
 	}
 
