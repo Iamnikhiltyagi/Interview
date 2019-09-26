2ea223452349cf06c8c551f77c7db52c9d58f03d
vikas maurya <vikas.maurya@sstsinc.com>
2019-09-26 18:16:19 +0530

    Update StudentRegistration.java

--- InterView.Registration.Ui/src/Registration/StudentRegistration.java
+++ InterView.Registration.Ui/src/Registration/StudentRegistration.java
@@ -30,7 +30,6 @@ public class StudentRegistration {
 		Utility.emailValid(email);
 		System.out.println("Enter Address ");
 		String address = sc.nextLine();
-		System.out.println("some change occur");
 		while(address.isEmpty())
 		{
 			System.out.println("address can't be empty");
